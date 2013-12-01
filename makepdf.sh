7f40233b30132da540523a623411b6916545074a
mstsirkin <mstsirkin@0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652>
2013-12-01 21:44:38 +0000

    tex: rename file
    Rename virtio-v1.0-csd01.tex to virtio.tex,
    so we don't have to rename before use.
    Use --jobname for output.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    git-svn-id: https://tools.oasis-open.org/version-control/svn/virtio@140 0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652

--- makepdf.sh
+++ makepdf.sh
@@ -3,6 +3,6 @@
 SPECDOC=virtio-v1.0-csd01
 
 rm $SPECDOC.aux
-xelatex $SPECDOC.tex
-xelatex $SPECDOC.tex
-xelatex $SPECDOC.tex
+xelatex --jobname $SPECDOC virtio.tex
+xelatex --jobname $SPECDOC virtio.tex
+xelatex --jobname $SPECDOC virtio.tex
