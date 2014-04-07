287ebe2383513f6def095f16f5f950895e038d2a
mstsirkin <mstsirkin@0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652>
2014-04-07 11:30:46 +0000

    makepdf: remove out file, could be corrupted
    makes build more robust
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    git-svn-id: https://tools.oasis-open.org/version-control/svn/virtio@357 0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652

--- makepdf.sh
+++ makepdf.sh
@@ -3,7 +3,7 @@
 SPECDOC=${SPECDOC:-`cat REVISION`}
 ./make-setup-generated.sh "$SPECDOC"
 
-rm $SPECDOC.aux $SPECDOC.pdf
+rm $SPECDOC.aux $SPECDOC.pdf $SPECDOC.out
 xelatex --jobname $SPECDOC virtio.tex
 xelatex --jobname $SPECDOC virtio.tex
 xelatex --jobname $SPECDOC virtio.tex
