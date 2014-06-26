34cff3f85855c55014d6b200e2085754e94835ae
mstsirkin <mstsirkin@0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652>
2014-06-26 12:34:11 +0000

    makediffpdf.sh: tool to create marked-up diff
    make pdf diff using latexpand and latexdiff-fast
    
    styles are set in diffpreamble.tex
    
    in diff, links are coloured green instead of blue
    
    Must be run within a git-svn clone of the spec repository.
    
    Note: latexdiff has --flatten option, this and options
    to select diff style don't seem to work well.
    So flatten by script myself, and add our own preamble.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    git-svn-id: https://tools.oasis-open.org/version-control/svn/virtio@387 0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652

---
+++ makediffpdf.sh
@@ -0,0 +1,34 @@
+#make pdf diff using latexpand and latexdiff-fast
+#preamble in diffpreamble.tex
+#in diff, links are coloured green instead of blue
+PATH=.:${PATH}
+cur="$PWD"
+rm -fr old new
+git clone $PWD old
+cd "${cur}/old"
+git checkout ec1ffbf27a8f0a06ca65cd498a69c7f89bd97dc1
+mv specvars.tex specvars-orig.tex
+#make links green to avoid confusion
+sed s/blue/green/ specvars-orig.tex > specvars.tex
+SPECDOC=${SPECDOC:-`cat REVISION`}
+./make-setup-generated.sh "$SPECDOC"
+#wget http://www.ctan.org/pkg/latexpand
+#chmod +x latexpand
+latexpand virtio.tex -o flat.tex
+cd "${cur}"
+git clone $PWD new
+cd "${cur}/new"
+mv specvars.tex specvars-orig.tex
+#make links green to avoid confusion
+sed s/blue/green/ specvars-orig.tex > specvars.tex
+SPECDOC=${SPECDOC:-`cat REVISION`}
+./make-setup-generated.sh "$SPECDOC"
+latexpand virtio.tex -o flat.tex
+cd "${cur}"
+#wget http://mirror.math.ku.edu/tex-archive/support/latexdiff/latexdiff-fast
+#chmod +x latexdiff-fast
+latexdiff-fast -p diffpreamble.tex old/flat.tex new/flat.tex > virtio-diff.tex
+rm $SPECDOC.aux $SPECDOC.pdf $SPECDOC.out
+xelatex --jobname $SPECDOC virtio-diff.tex
+xelatex --jobname $SPECDOC virtio-diff.tex
+xelatex --jobname $SPECDOC virtio-diff.tex
