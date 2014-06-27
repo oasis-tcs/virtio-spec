77f6b2842d2319e47addabcf44d3f66c65fd8482
mstsirkin <mstsirkin@0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652>
2014-06-27 16:10:24 +0000

    makediffall.sh: script to build all, include diff
    git-svn-id: https://tools.oasis-open.org/version-control/svn/virtio@403 0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652

---
+++ makediffall.sh
@@ -0,0 +1,14 @@
+export SPECDOC=${SPECDOC:-`cat REVISION`}
+./makezip.sh
+./makehtml.sh
+./makediffhtml.sh
+./makepdf.sh
+./makediffpdf.sh
+zip $SPECDOC.zip $SPECDOC.pdf $SPECDOC-diff.html $SPECDOC-diff.css $SPECDOC-diff.pdf
+echo Generated file $SPECDOC.zip
+echo To change output file name, set SPECDOC environment variable
+echo Examples:
+echo     SPECDOC=virtio-v1.0-wd01 $0
+echo     SPECDOC=virtio-v1.0-csd01 $0
+echo     SPECDOC=virtio-v1.0-csprd01 $0
+echo     SPECDOC=virtio-v1.0-os $0
