70c19d1938b4310efcb3423baca272e1dd264c2c
mstsirkin <mstsirkin@0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652>
2014-10-19 10:18:36 +0000

    makeall: document cs as option
    it's already supported, just undocumented
    
    git-svn-id: https://tools.oasis-open.org/version-control/svn/virtio@428 0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652

--- makeall.sh
+++ makeall.sh
@@ -9,4 +9,5 @@ echo Examples:
 echo     SPECDOC=virtio-v1.0-wd01 $0
 echo     SPECDOC=virtio-v1.0-csd01 $0
 echo     SPECDOC=virtio-v1.0-csprd01 $0
+echo     SPECDOC=virtio-v1.0-cs01 $0
 echo     SPECDOC=virtio-v1.0-os $0
