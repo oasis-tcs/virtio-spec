220bd6d578b0842912997febb8841cc05995814e
mstsirkin <mstsirkin@0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652>
2014-12-23 20:31:55 +0000

    makediff: cherry-pick more changelog commits
    We really should automate this going forward.
    For now - cherry-pick manually.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    git-svn-id: https://tools.oasis-open.org/version-control/svn/virtio/branches/v1.0@468 0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652

--- makediff.sh
+++ makediff.sh
@@ -32,6 +32,7 @@ formatting: mark change manually as changed in cs02
 cl: remove changelog for cs01
 cl-os: prepare changelog for v1.0 cs02
 title: update previous version to cs01
+changelog: list acknowledgement change
 EOF
 
 #mv specvars.tex specvars-orig.tex
