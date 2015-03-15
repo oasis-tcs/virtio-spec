c9873341c915886263023cd7e30320c5c9736c6f
mstsirkin <mstsirkin@0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652>
2015-03-15 11:10:30 +0000

    makediff: update for cs03
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    git-svn-id: https://tools.oasis-open.org/version-control/svn/virtio/branches/v1.0@483 0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652

--- makediff.sh
+++ makediff.sh
@@ -9,31 +9,17 @@ export DATESTR=${DATESTR:-`cat REVISION-DATE`}
 MAIN=$1
 PATH=.:${PATH}
 cur="$PWD"
-oldrev=`git rev-list -1 origin/tags/v1.0-cs01`
+oldrev=`git rev-list -1 origin/tags/v1.0-cs02`
 newrev=`git rev-list -1 HEAD`
 rm -fr old new
 git clone $PWD old
 cd "${cur}/old"
 git checkout $oldrev
-##suppress diff of title
-#git cherry-pick 0adee486ab987c3e98c5f31b51cc963d8bb6fff4
-##suppress diff of changelog
-#git cherry-pick a41f3813a748e7d279cb6eb82f3c0afde4a3243a
-#git cherry-pick fbfb402e69cdd9279c44b7684612e6f81df99e6d
-#git cherry-pick 9f240fe0e718bf9b1e502e02916db9d8fede304b
-#git cherry-pick a02605f9945f450ecaadf86736741de2e2c2e788
-#git cherry-pick 175e797beede8aea840102bee9b70bb08190153d
 while read -r rev; do
 	echo "Applying $rev"
 	git cherry-pick `git rev-list -1 -F --grep "$rev" $newrev` || exit 1
 done << 'EOF'
-formatting: escape \ldots in lstlisting
-formatting: mark change manually as changed in cs02
-cl: remove changelog for cs01
-cl-os: prepare changelog for v1.0 cs02
-title: update previous version to cs01
-changelog: list acknowledgement change
-changelog: typo fixup: formatting: formatting
+Revert: formatting: mark change manually as changed in cs02
 EOF
 
 #mv specvars.tex specvars-orig.tex
