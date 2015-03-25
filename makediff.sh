ab5fb8dd72b80f7587d03a0a9b4aac5738f56998
mstsirkin <mstsirkin@0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652>
2015-03-25 17:09:28 +0000

    makediff: cherry pick cl changes
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    git-svn-id: https://tools.oasis-open.org/version-control/svn/virtio/branches/v1.0@496 0c8fb4dd-22a2-4bb5-bc14-6c75a5f43652

--- makediff.sh
+++ makediff.sh
@@ -20,6 +20,8 @@ while read -r rev; do
 	git cherry-pick `git rev-list -1 -F --grep "$rev" $newrev` || exit 1
 done << 'EOF'
 Revert: formatting: mark change manually as changed in cs02
+cl: move out cs02 changelog
+cl: drop contents temporarily
 EOF
 
 #mv specvars.tex specvars-orig.tex
