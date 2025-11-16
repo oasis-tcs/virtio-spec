f6c548ff7182aaf079cecae1b1990068982f829d
Parav Pandit <parav@nvidia.com>
2025-11-16 05:16:50 +0200

    editorial: Remove hyperlink with hash tag
    The new hyperlink contains the hash letter.
    The new replaced link breaks the diff generation.
    
    This is because diff operation generates DIFadd, DIFdel
    commands inside the href.
    
    This causes deep recursive macro expansion resulting into
    below error.
    Avoid this small change in the diff generation.
    
    Remove the other patches which already exists now in the
    master branch.
    
    ! TeX capacity exceeded, sorry [input stack size=10000].
    
    \@setfontsize #1#2#3->\@nomath #1 \ifx \protect
    
    \@typeset@protect \let \@curr... l.577 ...delines/tc-process#wpComponentsCompLang}}
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>

--- makediff.sh
+++ makediff.sh
@@ -20,9 +20,7 @@ while read -r rev; do
 	echo "Applying $rev"
 	git cherry-pick --keep-redundant-commits --allow-empty `git rev-list -1 --grep "^$rev" $newrev` || exit 1
 done << 'EOF'
-editorial: allow for longer device id table: makediff 1.3
-admin: Prepare table for multipage listing
-admin: switch to tabularx
+editorial: update "Computer Language Definitions" URL
 EOF
 
 #mv specvars.tex specvars-orig.tex
